bb972a93edd40b51fee5872497c7308e4b36510f
didiergamassa <108032706+didiergamassa@users.noreply.github.com>
2024-04-23 15:22:40 +0200

    com3

--- src/main.js
+++ src/main.js
@@ -1,6 +1,5 @@
 import { createApp } from "vue";
 import App from "./App.vue";
-import router from "./router/index.js"; // Chemin vers ton fichier router/index.js
 
 createApp(App)
   .use(router) // Utiliser Vue Router dans l'application
